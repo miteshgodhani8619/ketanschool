36d0af6750920506876f487c832306beb4da6f3b
MItesh Godhani <miteshgodhani85@gmail.com>
2023-12-20 11:49:39 +0530

    Completed Home Page For Browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,45 @@ jQuery(document).ready(function(){
 
 
 // Header Sticky
-$(window).on('scroll',function() {
+jQuery(window).on('scroll',function() {
 
-	if ($(this).scrollTop() > 120){  
+	if (jQuery(this).scrollTop() > 120){  
 
-		$('header').addClass("is-sticky");
+		jQuery('header').addClass("is-sticky");
 
 	}
 	else{
 
-		$('header').removeClass("is-sticky");
+		jQuery('header').removeClass("is-sticky");
 	}
 });
-
+	
+	jQuery('.owl-carousel').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		autoplayHoverPause: true,
+		autoplay: true,
+		mouseDrag: true,
+		navText: [
+			"<i class='bx bx-chevron-left'></i>",
+			"<i class='bx bx-chevron-right'></i>"
+		],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 2,
+			},
+			768: {
+				items: 2,
+			},
+			1200: {
+				items: 3,
+			},
+		}
+	})
+	jQuery('.owl-item').addClass("col-md-3 col-6");
 
 });
